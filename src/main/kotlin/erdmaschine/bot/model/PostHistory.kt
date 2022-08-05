259c05bf862599892d5a2a075399f5a7f4a9fa58
ApoY2k <72788769+ApoY2k@users.noreply.github.com>
2022-08-05 22:12:25 +0200

    Add default value for post history entries

--- src/main/kotlin/erdmaschine/bot/model/PostHistory.kt
+++ src/main/kotlin/erdmaschine/bot/model/PostHistory.kt
@@ -1,5 +1,6 @@
 package erdmaschine.bot.model
 
+import kotlinx.datetime.Clock
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
@@ -8,7 +9,7 @@ object PostHistory : Table() {
     val channelId = varchar("channelId", 100)
     val sub = varchar("sub", 100)
     val postId = varchar("postId", 100)
-    val lastSeen = timestamp("lastSeen")
+    val lastSeen = timestamp("lastSeen").default(Clock.System.now())
 
     override val primaryKey = PrimaryKey(guildId, channelId, sub, postId)
 }
