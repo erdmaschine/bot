b45826d3e3e94988ac9d446e707c62ea467ded78
r-netizen <56514742+r-netizen@users.noreply.github.com>
2024-04-28 16:21:34 +0200

    Create XkcdComics.kt

---
+++ src/main/kotlin/erdmaschine/bot/model/XkcdComics.kt
@@ -0,0 +1,14 @@
+package erdmaschine.bot.model
+
+val XkcdComics = mapOf(
+    listOf("standards") to "https://imgs.xkcd.com/comics/standards.png",
+    listOf("usb", "kabel") to "https://imgs.xkcd.com/comics/usb_cables.png",
+    listOf("sudo", "sandwich") to "https://xkcd.com/149/",
+    listOf("reviews") to "https://xkcd.com/1036/",
+    listOf("compiling") to "https://xkcd.com/303/",
+    listOf("workflow") to "https://xkcd.com/1172/",
+    listOf("perseids", "pronunciation") to "https://xkcd.com/2814/",
+    listOf("laptopissues") to "https://xkcd.com/2083/",
+    listOf("free") to "https://xkcd.com/641/",
+    listOf("littlebobbytables", "sql") to "https://xkcd.com/327/" 
+)
